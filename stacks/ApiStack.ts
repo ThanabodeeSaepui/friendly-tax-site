b4b7fda13e64c23a33a2c75a2a9f732bffed9979
ThanabodeeSaepui <s6301012620138@email.kmutnb.ac.th>
2024-01-30 20:35:42 +0700

    Add tailwind css & stacks

---
+++ stacks/ApiStack.ts
@@ -0,0 +1,31 @@
+import { Api, StackContext, use } from "sst/constructs";
+import { StorageStack } from "./StorageStack";
+
+export function ApiStack({ stack }: StackContext) {
+  const { table } = use(StorageStack);
+
+  // Create the API
+  const api = new Api(stack, "Api", {
+    defaults: {
+      function: {
+        bind: [table],
+      },
+    },
+    cors: {
+      allowMethods: ["GET"],
+    },
+    routes: {
+      "POST /notes": "packages/functions/src/create.main",
+    },
+  });
+
+  // Show the API endpoint in the output
+  stack.addOutputs({
+    ApiEndpoint: api.url,
+  });
+
+  // Return the API resource
+  return {
+    api,
+  };
+}
