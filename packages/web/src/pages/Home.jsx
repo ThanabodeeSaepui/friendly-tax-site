2a57d67ea258f0824ad1bb58f7594efc9d48c9ad
ThanabodeeSaepui <s6301012620138@email.kmutnb.ac.th>
2024-02-01 22:43:28 +0700

    Fix tailwind

--- packages/web/src/pages/Home.jsx
+++ packages/web/src/pages/Home.jsx
@@ -1,8 +1,8 @@
 export default function Home() {
     return (
-      <div style={{ padding: 20 }}>
-        <h2>Home View</h2>
+      <>
+        <h2 className="text-3xl font-bold underline">Home View</h2>
         <p>Lorem ipsum dolor sit amet, consectetur adip.</p>
-      </div>
+      </>
     );
 }
