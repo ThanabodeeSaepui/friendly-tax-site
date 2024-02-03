93177a4d34376d7b8c90eb694f5e4348c09d0aa5
ThanabodeeSaepui <s6301012620138@email.kmutnb.ac.th>
2024-02-03 22:31:31 +0700

    Add Menu

--- packages/web/src/App.jsx
+++ packages/web/src/App.jsx
@@ -1,12 +1,59 @@
 import { Routes, Route } from "react-router-dom";
+import { Layout, Menu } from 'antd';
+
 import Home from './pages/Home'
+import Profile from './pages/User/Profile'
+
 import './App.css'
+
+const { Header } = Layout;
+const items = [
+  {
+    key: 0, label: "Home"
+  },
+  {
+    key: 1, label: "About Us"
+  },
+  {
+    key: 2, label: "Tax calculator"
+  },
+  {
+    key: 3, label: "Tax reduction"
+  },
+]
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-    </Routes>
+    <>
+      <Layout>
+      <Header
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+        }}
+      >
+        <div className="demo-logo" />
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          defaultSelectedKeys={['2']}
+          items={items}
+          style={{
+            flex: 1,
+            minWidth: 0,
+          }}
+        />
+      </Header>
+    </Layout>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="user" element={<Profile />} >
+          <Route path="profile" element={<Profile />} />
+          {/* <Route path="account" element={<Account />} /> */}
+        </Route>
+      </Routes>
+    </>
+    
   )
 }
 
---
+++ packages/web/src/pages/User/Profile.jsx
@@ -0,0 +1,7 @@
+export default function Profile() {
+    return (
+      <>
+        <h2 className="text-3xl font-bold underline">Profile Page</h2>
+      </>
+    );
+}
