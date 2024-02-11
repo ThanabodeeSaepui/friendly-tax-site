12371ae88aa2474775768828843047d7d5bc9b7a
ThanabodeeSaepui <s6301012620138@email.kmutnb.ac.th>
2024-02-11 18:57:55 +0700

    Fix company profile layout

--- packages/web/src/pages/App/CompanyProfile.jsx
+++ packages/web/src/pages/App/CompanyProfile.jsx
@@ -3,29 +3,29 @@ export default function CompanyProfile() {
         <>
             <div className="card bg-base-100 shadow-xl p-6">
                 <h1 className="font-bold">BLUEBRIDGE</h1>
-                <div className="flex">
+                <div className="flex justify-between">
                     <p className="text-left font-bold">เลขประจำตัวผู้เสียภาษี</p>
                     <p className="text-right">111888444556</p>
                 </div>
-                <div>
+                <div className="flex justify-between">
                     <p className="text-left font-bold">วันครบรอบปิดบัญชี</p>
-                    <p>01/01/2024</p>
+                    <p className="text-right">01/01/2024</p>
                 </div>
-                <div>
+                <div className="flex justify-between">
                     <p className="text-left font-bold">ประเภทธุรกิจ</p>
-                    <p>SME</p>
+                    <p className="text-right">SME</p>
                 </div>
-                <div>
+                <div className="flex justify-between">
                     <p className="text-left font-bold">รูปแบบการเสียภาษี</p>
-                    <p>นิติบุคคล</p>
+                    <p className="text-right">นิติบุคคล</p>
                 </div>
-                <div>
+                <div className="flex justify-between">
                     <p className="text-left font-bold">ระยะเวลายื่นภาษีประจำปี</p>
-                    <p>01/01/2024 - 30/05/2024</p>
+                    <p className="text-right">01/01/2024 - 30/05/2024</p>
                 </div>
-                <div>
+                <div className="flex justify-between">
                     <p className="text-left font-bold">ระยะเวลายื่นภาษีครึ่งปี</p>
-                    <p>30/06/2024 - 30/08/2024</p>
+                    <p className="text-right">30/06/2024 - 30/08/2024</p>
                 </div>
                 <div>
                     <button className="btn btn-active ">แก้ไข</button>
