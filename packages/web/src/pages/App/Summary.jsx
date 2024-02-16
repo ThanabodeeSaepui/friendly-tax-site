5e2c0e36af5f8299bf4124f1f31b61a9a6013fc4
ThanabodeeSaepui <s6301012620138@email.kmutnb.ac.th>
2024-02-16 17:41:48 +0700

    Add style to Summary page

--- packages/web/src/pages/App/Summary.jsx
+++ packages/web/src/pages/App/Summary.jsx
@@ -9,55 +9,55 @@ export default function Summary() {
           <hr className="mx-[4%] my-4 h-0.5 w-[10%] border-t-0 bg-secondary opacity-100" />
         </div>
         <h2 className="font-bold text-2xl">ค่าลดหย่อน</h2>
-        <div className="ml-[4%] my-10">
-          <div>
-            <h4>ค่าลดหย่อนส่วนตัว</h4>
+        <div className="ml-[4%] my-5">
+          <div className="text-left">
+            <h4 className="font-bold text-xl">ค่าลดหย่อนส่วนตัว</h4>
             <p>60,000 บาท</p>
           </div>
-          <div>
-            <h4>ค่าลดหย่อนบิดามารดา</h4>
+          <div className="text-left">
+            <h4 className="font-bold text-xl">ค่าลดหย่อนบิดามารดา</h4>
             <p>คนละ 30,000 บาท</p>
           </div>
-          <div>
-            <h4>ค่าลดหย่อนบุตร</h4>
+          <div className="text-left">
+            <h4 className="font-bold text-xl">ค่าลดหย่อนบุตร</h4>
             <p>
               ลูกคนแรกแบบเหมา 30,000 บาทต่อปี <br />
               ลูกคนที่ 2 เป็นต้นไป (เกิดหลัง 2561) คนละ 60,000 บาท
             </p>
           </div>
-          <div>
-            <h4>เงินประกันสังคม</h4>
+          <div className="text-left">
+            <h4 className="font-bold text-xl">เงินประกันสังคม</h4>
             <p>จ่ายตามจริงแต่ไม่เกิน 9,000 บาท</p>
           </div>
-          <div>
-            <h4>กองทุนการออมแห่งชาติ</h4>
+          <div className="text-left">
+            <h4 className="font-bold text-xl">กองทุนการออมแห่งชาติ</h4>
             <p>
               จ่ายตามจริงแต่ไม่เกิน 30,000 บาทและเมื่อรวมกับ 10. และ RMF <br />
               และเบี้ยประกันชีวิตแบบบำนาญ แล้วต้องไม่เกิน 500,000 บาท
             </p>
           </div>
-          <div>
-            <h4>เงินประกันสังคม</h4>
+          <div className="text-left">
+            <h4 className="font-bold text-xl">เงินประกันสังคม</h4>
             <p>จ่ายตามจริงแต่ไม่เกิน 9,000 บาท</p>
           </div>
-          <div>
-            <h4>กองทุนการออมแห่งชาติ</h4>
+          <div className="text-left">
+            <h4 className="font-bold text-xl">กองทุนการออมแห่งชาติ</h4>
             <p>
               จ่ายตามจริงแต่ไม่เกิน 30,000 บาทและเมื่อรวมกับ 10. และ RMF
               <br />
               และเบี้ยประกันชีวิตแบบบำนาญ แล้วต้องไม่เกิน 500,000 บาท
             </p>
           </div>
-          <div>
-            <h4>กองทุนการออมแห่งชาติ</h4>
+          <div className="text-left">
+            <h4 className="font-bold text-xl">กองทุนการออมแห่งชาติ</h4>
             <p>
               จ่ายตามจริงแต่ไม่เกิน 30,000 บาทและเมื่อรวมกับ 10. และ RMF
               <br />
               และเบี้ยประกันชีวิตแบบบำนาญ แล้วต้องไม่เกิน 500,000 บาท
             </p>
           </div>
-          <div>
-            <h4>ค่าซื้อกองทุนเพื่อการออม SSF</h4>
+          <div className="text-left">
+            <h4 className="font-bold text-xl">ค่าซื้อกองทุนเพื่อการออม SSF</h4>
             <p>
               ตามที่จ่ายจริงแต่ไม่เกิน 30% ของเงินได้ที่ต้องเสียภาษีและไม่เกิน
               200,000 <br />
@@ -68,54 +68,56 @@ export default function Summary() {
           </div>
         </div>
         <h2 className="font-bold text-2xl">รายการยกเว้นภาษีแบบพิเศษ</h2>
-        <div className="ml-[4%] my-10">
-          <div>
-            <h4>
+        <div className="ml-[4%] my-5">
+          <div className="text-left my-3">
+            <h4 className="font-bold text-xl">
               ค่าเสื่อมคอมพิวเตอร์ อุปกรณ์ <br />
               และโปรแกรมคอมพิวเตอร์คอมพิวเตอร์
             </h4>
             <p>เอามาคิดได้ในอัตรา 40% ของมูลค่าทรัพย์สินส่วนที่เหลือ</p>
           </div>
-          <div>
-            <h4>ค่าระบบและอุปกรณ์ที่สนับสนุนระบบภาษีอิเล็กทรอนิกส์</h4>
+          <div className="text-left my-3">
+            <h4 className="font-bold text-xl">
+              ค่าระบบและอุปกรณ์ที่สนับสนุนระบบภาษีอิเล็กทรอนิกส์
+            </h4>
             <p>
               หักเป็นรายจ่ายได้ 2 เท่า แต่ไม่เกิน 100,000 บาท
               <br />
             </p>
             <p>มีหลักเกณฑ์ดังนี้</p>
-            <ul>
-              <li>
-                - ร้านค้านิติบุคคลที่ใช้ระบบ POS ในการคิดค่าสินค้าและบริการ
-              </li>
-              <li>- บริการระบบคลาวด์</li>
-              <li>- ค่าพัฒนาระบบ Withholding Tax</li>
-              <li>- E-tax invoices/ E-receipts</li>
+            <ul className="list-disc">
+              <li>ร้านค้านิติบุคคลที่ใช้ระบบ POS ในการคิดค่าสินค้าและบริการ</li>
+              <li>บริการระบบคลาวด์</li>
+              <li>ค่าพัฒนาระบบ Withholding Tax</li>
+              <li>E-tax invoices/ E-receipts</li>
             </ul>
           </div>
-          <div>
-            <h4>เงินบริจาค (บริจาคในนามของนิติบุคคล)</h4>
+          <div className="text-left my-3">
+            <h4 className="font-bold text-xl">
+              เงินบริจาค (บริจาคในนามของนิติบุคคล)
+            </h4>
             <p>บริจาคให้กับกองทุนส่งเสริมและพัฒนาคุณภาพชีวิตคนพิการ</p>
-            <ul>
+            <ul className="list-disc">
               <li>
                 หักรายจ่ายได้เท่ากับจำนวนเงินหรือมูลค่าที่บริจาคแต่ต้องไม่เกิน
                 2% ของกำไรสุทธิก่อนหักรายจ่าย
               </li>
             </ul>
             <p>บริจาคให้กับสถาบันการศึกษา</p>
-            <ul>
-              <li>- ได้ทางภาครัฐเอกชนและอุดมศึกษา</li>
-              <ul>
+            <ul className="list-disc">
+              <li>ได้ทางภาครัฐเอกชนและอุดมศึกษา</li>
+              <ul className="list-disc">
                 <li>
-                  - หักรายจ่ายได้สองเท่าแต่ต้องไม่เกิน 10%
+                  หักรายจ่ายได้สองเท่าแต่ต้องไม่เกิน 10%
                   <br />
                   ของกำไรสุทธิก่อนหักรายจ่ายเพื่อการกุศล
                 </li>
               </ul>
             </ul>
             <p>บริจาคให้กับกองทุนวิจัย พัฒนา และนวัตกรรม</p>
-            <ul>
+            <ul className="list-disc">
               <li>
-                - หักรายจ่ายได้สองเท่าของจำนวนเงินที่บริจาคเมื่อรวม
+                หักรายจ่ายได้สองเท่าของจำนวนเงินที่บริจาคเมื่อรวม
                 <br />
                 กับรายจ่ายที่เป็นค่าใช้จ่ายเพื่อการศึกษา
                 <br />
@@ -125,20 +127,20 @@ export default function Summary() {
               </li>
             </ul>
           </div>
-          <div>
-            <h4>ค่าระบบและอุปกรณ์ที่สนับสนุนระบบภาษีอิเล็กทรอนิกส์</h4>
+          <div className="text-left my-3">
+            <h4 className="font-bold text-xl">
+              ค่าระบบและอุปกรณ์ที่สนับสนุนระบบภาษีอิเล็กทรอนิกส์
+            </h4>
             <p>
               หักเป็นรายจ่ายได้ 2 เท่า แต่ไม่เกิน 100,000 บาท
               <br />
             </p>
             <p>มีหลักเกณฑ์ดังนี้</p>
-            <ul>
-              <li>
-                - ร้านค้านิติบุคคลที่ใช้ระบบ POS ในการคิดค่าสินค้าและบริการ
-              </li>
-              <li>- บริการระบบคลาวด์</li>
-              <li>- ค่าพัฒนาระบบ Withholding Tax</li>
-              <li>- E-tax invoices/ E-receipts</li>
+            <ul className="list-disc">
+              <li>ร้านค้านิติบุคคลที่ใช้ระบบ POS ในการคิดค่าสินค้าและบริการ</li>
+              <li>บริการระบบคลาวด์</li>
+              <li>ค่าพัฒนาระบบ Withholding Tax</li>
+              <li>E-tax invoices/ E-receipts</li>
             </ul>
           </div>
         </div>
